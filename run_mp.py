1d693ca0d9c1183647b5b10d08f33c0fd71561b9
Xander Wilcke <w.x.wilcke@vu.nl>
2019-07-16 18:18:40 +0200

    Added parallel support

---
+++ run_mp.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env 
+
+import argparse
+import os
+import pickle
+from time import time
+
+from rdflib import Graph
+from rdflib.util import guess_format
+
+from sequential import validate
+
+
+if __name__ == "__main__":
+    timestamp = int(time())
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--constraints", help="Constraints to validate against (.pkl)",
+            required=True)
+    parser.add_argument("-d", "--max_depth", help="Maximum depth to validate",
+            default=0)
+    parser.add_argument("-i", "--input", help="One or more RDF-encoded graphs",
+            required=True, nargs='+')
+    parser.add_argument("-n", "--nproc", help="Number of cores to utilize",
+            default=os.cpu_count())
+    args = parser.parse_args()
+
+    # load graph(s)
+    print("importing graphs... ")
+    g = Graph()
+    for gf in args.input:
+        g.parse(gf, format=guess_format(gf))
+
+    # load constraints
+    generation_forest = None
+    print("importing constraints... ")
+    with open(args.constraints, 'rb') as cf:
+        generation_forest = pickle.load(cf)
+
+    # validate
+    print("validating data... ")
+    h = validate(int(args.proc), g, generation_forest, args.max_depth)
+
+    # store results
+    print("storing results... ")
+    h.serialize("./constraint_violations.nt", format="nt")
--- sequential.py
+++ sequential.py
@@ -3,7 +3,8 @@
 from rdflib import BNode, Graph, Literal, RDF, RDFS
 
 from pgkgc.cache import Cache
-from pgkgc.structures import IdentityAssertion, DataTypeVariable, ObjectTypeVariable, TypeVariable
+from pgkgc.metrics import confidence_of as compute_satisfied
+from pgkgc.metrics import support_of as compute_domain
 
 
 def validate(g, generation_forest, max_depth):
@@ -11,155 +12,51 @@ def validate(g, generation_forest, max_depth):
     h = Graph()
 
     violations = set()
-    for t in generation_forest.types():
-        tree = generation_forest.get_tree(t)
+    for depth in range(max_depth):
+        for t in generation_forest.types():
+            # root clauses have all members of type t as domain 
+            domain = set(g.subjects(RDF.type, t)) if depth <= 0 else set()
 
-        for clause in tree.get(depth=0):
-            domain = g.subjects(RDF.type, t)
-            violations |= validate_recursion(clause,
-                                             domain,
-                                             cache,
-                                             max_depth)
+            tree = generation_forest.get_tree(t)
+            for clause in tree.get(depth=depth):
+                if depth <= 0 and clause.parent._satisfy_body is None:
+                    # set starting point if previously cleared
+                    clause.parent._satisfy_body = domain
+
+                violations |= validate_recursion(clause, cache)
 
     for s, p, o in violations:
         flag_triple(h, s, p, o)
 
     return h
 
-def validate_recursion(clause, parent_domain, cache, depth):
-    if depth < 0:
-        return set()
+def validate_recursion(clause, cache):
+    if clause._satisfy_body is None:
+        # compute domain from that of parent to reduce search space
+        _, domain = compute_domain(cache.predicate_map,
+                                   cache.object_type_map,
+                                   cache.data_type_map,
+                                   clause.body,
+                                   clause.body.identity,
+                                   clause.parent._satisfy_body,
+                                   min_support=-1)
 
-    # reduce search space by constraining parent domain
-    domain = generate_domain(cache.predicate_map,
-                             cache.object_type_map,
-                             cache.data_type_map,
-                             clause.body,
-                             clause.body.identity,
-                             parent_domain)
+        clause._satisfy_body = domain
 
-    violations = check_violations(cache.predicate_map,
-                                  cache.object_type_map,
-                                  cache.data_type_map,
-                                  clause.head,
-                                  domain)
+
+    _, satisfied = compute_satisfied(cache.predicate_map,
+                                     cache.object_type_map,
+                                     cache.data_type_map,
+                                     clause.head,
+                                     clause._satisfy_body)
+
+    # domain entities that don't satisfy are violations
+    violations = clause._satisfy_body - satisfied
 
     for child in clause.children:
-        violations |= validate_recursion(child,
-                                         domain,
-                                         cache,
-                                         depth-1)
+        violations |= validate_recursion(child, cache)
 
     return violations
-
-def check_violations(predicate_map,
-                     object_type_map,
-                     data_type_map,
-                     assertion,
-                     assertion_domain):
-    """ Check whether a clause assertion holds within a domain of entities
-
-        Returns a set of violations
-    """
-    violations = set()
-    if not isinstance(assertion.rhs, TypeVariable):
-        # either an entity or literal
-        for entity in assertion_domain:
-            if assertion.rhs not in predicate_map[assertion.predicate]['forwards'][entity]:
-                # P(e, u) does not hold
-                violations.add(entity)
-    
-    elif isinstance(assertion.rhs, ObjectTypeVariable):
-        for entity in assertion_domain:
-            for resource in predicate_map[assertion.predicate]['forwards'][entity]:
-                if object_type_map['object-to-type'][resource] != assertion.rhs.type:
-                    # P(e, ?) with object type(?, t) does not hold
-                    violations.add(entity)
-
-    elif isinstance(assertion.rhs, DataTypeVariable):
-        for entity in assertion_domain:
-            for resource in predicate_map[assertion.predicate]['forwards'][entity]:
-                if data_type_map['object-to-type'][resource] != assertion.rhs.type:
-                    # P(e, ?) with data type(?, t) does not hold
-                    violations.add(entity)
-
-    return violations
-
-
-def generate_domain(predicate_map,
-                    object_type_map,
-                    data_type_map,
-                    context,
-                    assertion,
-                    assertion_domain):
-    """ Generate the domain given a graph context
-
-    Returns a set of entities
-    """
-    # no need to continue if we are a pendant incident (optimization)
-    if len(context.connections[assertion]) <= 0:
-        if isinstance(assertion, IdentityAssertion):
-            return assertion_domain
-
-        assertion_domain_updated = set()
-        if not isinstance(assertion.rhs, TypeVariable):
-            # either an entity or literal
-            for entity in assertion_domain:
-                if assertion.rhs in predicate_map[assertion.predicate]['forwards'][entity]:
-                    # P(e, u) holds
-                    assertion_domain_updated.add(entity)
-        elif isinstance(assertion.rhs, ObjectTypeVariable):
-            for entity in assertion_domain:
-                for resource in predicate_map[assertion.predicate]['forwards'][entity]:
-                    if object_type_map['object-to-type'][resource] == assertion.rhs.type:
-                        # P(e, ?) with object type(?, t) holds
-                        assertion_domain_updated.add(entity)
-        elif isinstance(assertion.rhs, DataTypeVariable):
-            for entity in assertion_domain:
-                for resource in predicate_map[assertion.predicate]['forwards'][entity]:
-                    if data_type_map['object-to-type'][resource] == assertion.rhs.type:
-                        # P(e, ?) with data type(?, t) holds
-                        assertion_domain_updated.add(entity)
-
-        return assertion_domain_updated
-
-    # retrieve range based on assertion's domain
-    if isinstance(assertion, IdentityAssertion):
-        assertion_range = assertion_domain
-    else:  # type is Clause.Assertion with ObjectTypeVariable as rhs
-        assertion_range = set()
-        for entity in assertion_domain:
-            for resource in predicate_map[assertion.predicate]['forwards'][entity]:
-                if object_type_map['object-to-type'][resource] is assertion.rhs.type:
-                    assertion_range.add(resource)
-
-    # update range based on connected assertions' domains (optimization)
-    for connection in context.connections[assertion]:
-        assertion_range &= frozenset(predicate_map[connection.predicate]['forwards'].keys())
-
-    # update range based on connected assertions' returned updated domains
-    # search space is reduced after each returned update
-    connection_domain = assertion_range  # only for readability
-    for connection in context.connections[assertion]:
-        range_update = generate_domain(predicate_map,
-                                       object_type_map,
-                                       data_type_map,
-                                       context,
-                                       connection,
-                                       connection_domain)
-
-        assertion_range &= range_update
-
-    # update domain based on updated range
-    if isinstance(assertion, IdentityAssertion):
-        return assertion_range
-
-    assertion_domain_updated = set()
-    for resource in assertion_range:
-        domain_update = predicate_map[assertion.predicate]['backwards'][resource]
-        assertion_domain_updated |= domain_update
-
-    return assertion_domain_updated
 
 def flag_triple(h, s, p, o):
     bnode = BNode()
